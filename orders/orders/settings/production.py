3f59e48d27b1d486a524b3bc28d42eb21ad4db36
Ninila <ninila_company@bk.ru>
2022-10-27 01:04:25 +0500

    edit static_root

--- orders/orders/settings/production.py
+++ orders/orders/settings/production.py
@@ -147,14 +147,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
 # Ckeditor collectstatic для сбора статики
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Путь для загрузки файлов
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
